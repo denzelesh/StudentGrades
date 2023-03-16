334c18ff3e04acb863c13a28ac1a1942f5abdd9a
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-03-16 08:44:49 +0000

    Added Grading Percentages & Main Method

--- program.cs
+++ program.cs
@@ -1,1 +1,14 @@
+class studentGrades
+{
+    // Grading Percentages 
+    const int gradeA = 70;
+    const int gradeB = 60;
+    const int gradeC = 50;
+    const int gradeD = 40;
+    const int gradeF = 0;
+
+     static void Main()
+    {
+    }
+}
 
