e949b0cafcf3712fdebf017b52af6d73cd804e88
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-03-16 13:57:44 +0000

    Added application heading

--- program.cs
+++ program.cs
@@ -6,9 +6,22 @@ class studentGrades
     const int gradeC = 50;
     const int gradeD = 40;
     const int gradeF = 0;
+    
+    static void ApplicationHeading()
+    {
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("   App 03 - Student Grades");
+        Console.WriteLine("         By Denzel Eshun");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("   This application will convert");
+        Console.WriteLine("   student marks to grades");
+        Console.WriteLine("   and provide a results summary");
+        Console.WriteLine("-----------------------------------");
+    }
 
      static void Main()
     {
+         ApplicationHeading();
     }
 }
 
